49592ceea16b5f45f8dddd2c3f50e93de6647950
xiaomimi2 <1585276817@qq.com>
2017-04-25 17:31:19 +0800

    4-25

--- src/js/common.js
+++ src/js/common.js
@@ -1,12 +1,17 @@
 var mycommonEvent = (function(){
 	//这里只支持a标签，可以优化一下去data-herf
-	function Tab(dom) {
+	function Tab(dom,fn) {
 		this.tab = dom;
 		this.id = this.tab.attr('href')
+		if(fn && typeof fn === 'function'){
+			this.fn = fn
+		}
 		this.switch()
 	}
 	Tab.prototype.switch = function(){
+		var that = this
 		var id = this.id
+		var render
 		this.tab.on('click',function(ev){
 			ev.preventDefault()
 			$(this).addClass('active')
@@ -15,13 +20,16 @@ var mycommonEvent = (function(){
 			$(id).show()
 					.siblings()
 					.hide();
+			if(that.fn){
+				render = getSingle(that.fn)
+				render()
+			}
 		})
 		
 	}
-
-
+	
 	function getSingle (fn) {
-		var result;
+		var result 
 		return function(){
 			if(!result) {
 				result = fn.apply(this,arguments)
@@ -105,15 +113,297 @@ var mycommonEvent = (function(){
 			}
 		}
 	}
+	//将video显示的实现格式化为00:00:00的格式
+	var formatVideoTime = function (time) {
+		var left = parseInt(time)
+		var hour = '00'
+		var minutes = '00'
+		
+
+		if(parseInt(time) > 3600) {
+			hour = behaveTowNumber(Math.floor(time / 3600))
+			left = time % 3600
+		}
+		if(left > 60) {
+			minutes = behaveTowNumber(Math.floor(left / 60))
+		}
+		if(left >= 0) {
+			var seconds = behaveTowNumber(left % 60)
+		}
+		return hour+':'+minutes+':'+seconds
+	}
+	function behaveTowNumber(num) {
+		if(num < 10){
+			return '0' + num 
+		}
+		return num + ''
+	}
+	//当前时间日期
+	function formdate () {
+		var date = new Date()
+		var year = date.getFullYear()
+		var month = behaveTowNumber(date.getMonth() + 1)
+		var day = behaveTowNumber(date.getDate())
+		var hour = behaveTowNumber(date.getHours())
+		var minutes = behaveTowNumber(date.getMinutes())
+		return year + '-' + month + '-' + day + '&nbsp;' + hour + ':' + minutes 
+	}
+
+	//瀑布流函数，这个适用于offsetparent是html的元素和window比较,然后比较元素的高度不用固定
+	function waterfallOfNotFIxedHeight(dom) {
+		var $dom = $(dom)
+		var wScroll = $(window).scrollTop()
+		var wClient = $(window).height()
+		var dHeight = $dom.height()
+		var doffsetTop = $dom.offset().top
+		if(dHeight +  doffsetTop < wScroll + wClient + 500){
+			return true
+		}else {
+			return false
+		} 
+	}
+
+	function Test(videourl,videouser,videopwd,roomid,realname) {
+        var result = "ystpy://" + videourl+ "," + videouser+ "," +videopwd+ "," +roomid+"," +realname;
+        location.href = result;
+    }
 
 	return {
 		Tab: Tab,
 		getSingle: getSingle,
 		saveSearchResult: saveSearchResult,
 		videoFactory: videoFactory,
-		Validator: Validator
+		Validator: Validator,
+		formatVideoTime:formatVideoTime,
+		waterfallOfNotFIxedHeight:waterfallOfNotFIxedHeight,
+		formdate:formdate,
+		openNet:Test
 	}
 })()
- $('.searchBar .icon-sousuo-sousuo').on('click', function(){
-            window.location.href="search.html";
-  })
+
+//每页都有搜索框的话。这个后期要加到每页去
+$(function(){
+	//checkLogined
+	if(localStorage.getItem('USER')) {
+		var user = JSON.parse(localStorage.getItem('USER'))
+		$('.loginer').find('img').attr('src',user.img)
+		$('.loginer').find('a:eq(1)').text(user.realname)
+	}else {
+		window.location.href = "enter.html"
+	}
+})
+ 
+// const INVITED_PEOPLE
+// const COMMENTS
+// const NOTES
+//假数据笔记
+var user
+if(localStorage.getItem('USER')){
+	user = JSON.parse(localStorage.getItem('USER'))
+}
+var dataComments = [{
+    name:'张三',
+    content:'努力加油啊',
+    videotime:2,
+    date: '2017-4-9&nbsp;15:30',
+    img:'image/user1.jpg'
+},
+{
+    name:'李四',
+    content:'老师的声音很好听，哈哈哈',
+    videotime:3,
+    date: '2017-4-8&nbsp;16:30',
+    img:'image/user2.jpg'
+},
+ {
+    name:'小吴',
+    content:'这个课程给我了很多帮助多谢老师的教导',
+    videotime:16,
+    date: '2017-4-7&nbsp;15:30',
+    img:'image/user1.jpg'
+},
+{
+    name:'小红',
+    content:'这一点讲得非常的到位，可以从中学到很多。来得及发了房间辣会计分录看得开了罚款了房间打开',
+    videotime:11,
+    date: '2017-4-3&nbsp;15:30',
+    img:'image/user2.jpg'
+},
+{
+    name:'小郭',
+    content:'既返垃圾分类会计分录卡死机离开健康路附近大户等方式里地福建省地附近的司法急急急',
+    videotime:1,
+    date: '2017-3-3&nbsp;15:30',
+    img:'image/user4.jpg'
+},
+{
+    name:'李四',
+    content:'老师的声音很好听，哈哈哈',
+    videotime:15,
+    date: '2017-4-8&nbsp;16:30',
+    img:'image/user2.jpg'
+},
+ {
+    name:'王五',
+    content:'这个课程给我了很多帮助多谢老师的教导',
+    videotime:11,
+    date: '2017-4-7&nbsp;15:30',
+    img:'image/user3.jpg'
+},
+{
+    name:'小红',
+    content:'这一点讲得非常的到位，可以从中学到很多。来得及发了房间辣会计分录看得开了罚款了房间打开',
+    videotime:20,
+    date: '2017-4-3&nbsp;15:30',
+    img:'image/user2.jpg'
+},
+{
+    name:'小郭',
+    content:'既返垃圾分类会计分录卡死机离开健康路附近大户等方式里地福建省地附近的司法急急急',
+    videotime:22,
+    date: '2017-3-3&nbsp;15:30',
+    img:'image/user4.jpg'
+}
+
+]
+
+var dataNotes = [{
+    videotime:12,
+    date: '2017-4-8&nbsp;16:30',
+    content:'笔记测试1',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:14,
+    date: '2017-4-8&nbsp;16:54',
+    content:'笔记测试2',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:25,
+    date: '2017-4-8&nbsp;13:20',
+    content:'笔记测试3',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:36,
+    date: '2017-4-8&nbsp;15:15',
+    content:'笔记测试4',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:48,
+    date: '2017-4-8&nbsp;16:30',
+    content:'笔记测试5',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:12,
+    date: '2017-4-8&nbsp;16:30',
+    content:'笔记测试1',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:14,
+    date: '2017-4-8&nbsp;16:54',
+    content:'笔记测试2',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:25,
+    date: '2017-4-8&nbsp;13:20',
+    content:'笔记测试3',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:36,
+    date: '2017-4-8&nbsp;15:15',
+    content:'笔记测试4',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:48,
+    date: '2017-4-8&nbsp;16:30',
+    content:'笔记测试5',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:12,
+    date: '2017-4-8&nbsp;16:30',
+    content:'笔记测试1',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:14,
+    date: '2017-4-8&nbsp;16:54',
+    content:'笔记测试2',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:25,
+    date: '2017-4-8&nbsp;13:20',
+    content:'笔记测试3',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:36,
+    date: '2017-4-8&nbsp;15:15',
+    content:'笔记测试4',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:48,
+    date: '2017-4-8&nbsp;16:30',
+    content:'笔记测试5',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:12,
+    date: '2017-4-8&nbsp;16:30',
+    content:'笔记测试1',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:14,
+    date: '2017-4-8&nbsp;16:54',
+    content:'笔记测试2',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:25,
+    date: '2017-4-8&nbsp;13:20',
+    content:'笔记测试3',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:36,
+    date: '2017-4-8&nbsp;15:15',
+    content:'笔记测试4',
+    img: user.img,
+    name:user.realname
+},
+{
+    videotime:48,
+    date: '2017-4-8&nbsp;16:30',
+    content:'这是结尾',
+    img: user.img,
+    name:user.realname
+}]
--- src/js/invite.js
+++ src/js/invite.js
@@ -3,28 +3,86 @@ $(function(){
     
     //对象池管理li
     var LiFactory 
-    //加上click事件增加
+    //加上click事件增加人选
     $('.add').on('click',function(ev){
         createLi = function(){
             var li = document.createElement('li')
             return li;
         }
+        //创建li对象
         LiFactory = mycommonEvent.videoFactory(createLi)
         var liDom = LiFactory.create()
         var imgurl = $(this).siblings('img').attr('src')
-        var name = $(this).siblings('span').text()
+        var name = $(this).siblings('.name').text()
+        console.log(name)
+        localStore.save(liDom,imgurl,name)
+        //绑定删除人选
         liDom.innerHTML = '<img src="'+imgurl+'"><span>'+name+'</span><span class="del">删除</span>'
         $(liDom).on('click', function(){
-            $(this.remove())
+            $(this.remove());
+            LiFactory.recover(this)
+            //这里用名字做标识符，后期需要改正！！！！！！！！！！
+            console.log($(this).find('span:eq(0)').text())
+            localStore.remove($(this).find('span:eq(0)').text())
         })
         $('.choosed-teacher').append(liDom)
     })
 
-    $('#invite').one('click',function(){
+    $('#invite').on('click',function(){
         if(window.location.pathname === '/invite.html') {
-            window.location.href = 'course.html'
+            saveTolocalStorage('INVITED_PEOPLE',localStore.invites())
+            window.location.href = 'course.html';
         }else{
+            saveTolocalStorage('INVITED_PEOPLE',localStore.invites())
             window.location.href = 'course.html?live=1'
         }
     })
+
+    function saveTolocalStorage(name, data){
+        if(typeof data === 'object') {
+            data = JSON.stringify(data)
+        }
+        if(typeof name !== 'string') {
+            name = name + ''
+        }
+        localStorage.removeItem(name)
+        localStorage.setItem(name ,data)
+    }
+    
+
+    //这里暂且用名字作为唯一标识符。实际上应该会有一个userid！！！！invites只可读取，不可外边设置
+    var localStore = (function  () {
+        var invites = {}
+        function save(dom,imgurl,name) {
+            if(!invites[name]) {
+                invites[name] = {}
+                invites[name].img = imgurl
+                invites[name].dom = dom
+            }
+        }
+        function remove(name)  {
+            if(!invites[name]) {
+                return false 
+            }
+            delete invites[name]
+        }
+        //这里我已经设置的默认的二级层级，所以直接两级递归
+        function getInvites() {
+           var obj = {}
+           for(var i in invites) {
+                obj[i] = {}
+                for(var j in invites[i]) {
+                    obj[i][j] =  invites[i][j]
+                }
+           }
+           return obj;
+        }
+        return {
+            save: save,
+            remove: remove,
+            invites:getInvites
+        }
+
+    })()
+
 })
